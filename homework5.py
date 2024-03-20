8472ebb5012128eb20916da0ca627ab73dc2b74d
bugogat <162794631+bugogat@users.noreply.github.com>
2024-03-20 12:12:47 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,12 @@
+my_list = ['apple', 'banana', 'coconut', 'pineapple', 'orange', 'pear']
+print(my_list)
+print(my_list[0::5])
+print(my_list[2:5])
+my_list[3] = 'peach'
+print(my_list)
+my_dict = {'apple': 'яблоко', 'banana': 'банан', 'coconut': 'кокос', 'pineapple': 'ананас', 'orange': 'апельсин',
+          'pear': 'персик'}
+print(my_dict)
+print(my_dict.values())
+print(my_dict.update({'mango': 'манго'}))
+print(my_dict)
